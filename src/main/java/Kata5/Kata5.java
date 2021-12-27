45e49518f4f3d04151206a92bef739b6341b1e2e
airam <airam@DESKTOP-J9OODA7>
2021-12-27 14:40:58 +0000

    Kata5P1	 versión 1

---
+++ src/main/java/Kata5/Kata5.java
@@ -0,0 +1,22 @@
+
+package Kata5;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+public class Kata5 {
+
+        
+    public static void main(String[] args) {
+        SelectApp app = new SelectApp();
+        app.selectAll();
+               
+    }
+    
+    
+    
+}
+
+
